2e18c8abc7d8753c111cebad1ba0b31ce7c4ec5c
developer-ajay <developer.ajay618@gmail.com>
2025-02-28 11:54:32 +0530

    NOT operator

---
+++ HTML&JS/index.js
@@ -0,0 +1,7 @@
+let pageTitle = document.getElementById("page-title");
+let pageTitleContent = ""
+
+if (pageTitle && !pageTitleContent){    
+        pageTitleContent ="IPL 2025";
+        pageTitle.innerText = pageTitleContent;   
+}
---
+++ NOT_operator.js
@@ -0,0 +1,7 @@
+let thisIsATrueValue = false;
+console.log(thisIsATrueValue);
+
+
+if (!thisIsATrueValue){
+    console.log("Please execute me!!");
+}
