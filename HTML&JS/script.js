ab3efb023bbd226718190187a2207651eaf75cfd
developer-ajay <developer.ajay618@gmail.com>
2025-03-01 12:29:53 +0530

    fn declaration & expression

---
+++ HTML&JS/script.js
@@ -0,0 +1,16 @@
+
+//function declaration
+
+displayMessage();
+
+function displayMessage (){
+    console.log("Hello World");
+}
+
+//function expression
+
+const message = function (){
+    console.log("My message is  Hello World!")
+}
+
+message();
