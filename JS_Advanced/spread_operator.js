b568d0d5c20c1c57105be6b551e1234c8323fa5e
developer-ajay <developer.ajay618@gmail.com>
2025-03-01 18:34:55 +0530

    spread operator more functionality

--- JS_Advanced/spread_operator.js
+++ JS_Advanced/spread_operator.js
@@ -22,3 +22,32 @@ const players2 = ["imthiyaz","mohammed","fathima"];
 const all_players = [...players1,"adzyn",...players2]
 
 console.log(all_players);
+
+//spreading an array
+const numbers = [4,5,6]
+const whole_numbers = [1,2,3,...numbers]
+console.log(whole_numbers);
+
+//copying an object/array
+const languages = ["Java" , "Javascript"]
+const copy_languages = [...languages]
+console.log(copy_languages);
+
+
+//spreading object
+const personal_details = {
+    name : "Dona Ajay",
+    age : 26,
+    place : "Kannur"
+}
+const new_updates ={
+    ...personal_details , post : "Ulikkal"
+}
+console.log(new_updates)
+
+//spreading strings
+const greetings = "Hello World";
+const greet_object = {...greetings}
+const greet_array = [...greetings]
+console.log(greet_array)
+console.log(greet_object)
