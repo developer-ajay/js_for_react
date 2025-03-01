3ff52821b19c2560bcc5889f5eda3ce9216b0f38
developer-ajay <developer.ajay618@gmail.com>
2025-03-01 20:44:53 +0530

    arrow_functions

---
+++ JS_Advanced/arrow_function.js
@@ -0,0 +1,11 @@
+//ARROW FUNCTION
+
+const myFunction =  (num1, num2) => num1 + num2
+
+sum = myFunction(100,1000);
+console.log(sum);
+
+
+const multiply = (n1,n2,n3) => console.log(n1*n2*n3);
+
+multiply(19,56,89);
